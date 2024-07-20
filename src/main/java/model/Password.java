c18471382ef456a11afdc123f5723e40858b3380
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-07-20 08:22:48 -0300

    move change password logic to Password class

--- src/main/java/model/Password.java
+++ src/main/java/model/Password.java
@@ -1,11 +1,7 @@
 package model;
 
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import model.api.BusinessException;
 
 @Embeddable
@@ -14,16 +10,45 @@ import model.api.BusinessException;
 @Getter(value = AccessLevel.PRIVATE)
 @EqualsAndHashCode(of = {"password"})
 class Password {
-	private String password;
-	static final String NOT_VALID_PASSWORD = "Password is not valid";
+    private String password;
+    static final String NOT_VALID_PASSWORD = "Password is not valid";
+    static final String CAN_NOT_CHANGE_PASSWORD = "Some of the provided information is not valid to change the password";
+    static final String PASSWORDS_MUST_BE_EQUALS = "Passwords must be equals";
 
-	public Password(String password) {
-		String pwd = new NotBlankString(password, NOT_VALID_PASSWORD).value();
-		if (pwd.length() < 12) {
-			throw new BusinessException(NOT_VALID_PASSWORD);
-		}
-		// hash password before assign !
-		this.password = password;
-	}
+    public Password(String password) {
+        checkValidPassword(password);
+        this.password = encript(password);
+    }
 
+    private String encript(String nonEncriptedPassword) {
+        // encript password here !
+        return nonEncriptedPassword;
+    }
+
+    private void checkValidPassword(String password) {
+        String pwd = new NotBlankString(password, NOT_VALID_PASSWORD).value();
+        if (pwd.length() < 12) {
+            throw new BusinessException(NOT_VALID_PASSWORD);
+        }
+    }
+
+    public void change(String currentPassword, String newPassword1,
+                       String newPassword2) {
+        checkValidPassword(newPassword1);
+        if (!this.password.equals(currentPassword)) {
+            throw new BusinessException(CAN_NOT_CHANGE_PASSWORD);
+        }
+        checkPasswordsMatch(newPassword2, newPassword1);
+        this.password = newPassword1;
+    }
+
+    void checkPasswordsMatch(String password, String repeatPassword) {
+        if (!password.equals(repeatPassword)) {
+            throw new BusinessException(PASSWORDS_MUST_BE_EQUALS);
+        }
+    }
+
+    boolean hasPassword(String password) {
+        return this.password.equals(password);
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,26 +1,13 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import model.api.BusinessException;
+import model.api.UserProfile;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import model.api.BusinessException;
-import model.api.UserProfile;
 
 @Entity
 @Table(name = "ClientUser")
@@ -30,104 +17,91 @@ import model.api.UserProfile;
 @EqualsAndHashCode(of = {"userName"})
 public class User {
 
-	static final String INVALID_USERNAME = "A valid username must be provided";
-	static final String CAN_NOT_CHANGE_PASSWORD = "Some of the provided information is not valid to change the password";
-	static final String POINTS_MUST_BE_GREATER_THAN_ZERO = "Points must be greater than zero";
-	static final String PASSWORDS_MUST_BE_EQUALS = "Passwords must be equals";
+    static final String INVALID_USERNAME = "A valid username must be provided";
+    static final String POINTS_MUST_BE_GREATER_THAN_ZERO = "Points must be greater than zero";
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	@Column(unique = true)
-	private String userName;
-	@OneToOne(cascade = CascadeType.PERSIST)
-	private Person person;
-	// password must not escape by any means out of this object
-	@Embedded
-	private Password password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true)
+    private String userName;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Person person;
+    // password must not escape by any means out of this object
+    @Embedded
+    private Password password;
 
-	@OneToMany(cascade = CascadeType.PERSIST, mappedBy = "purchaser")
-	private List<Sale> purchases;
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "purchaser")
+    private List<Sale> purchases;
 
-	private int points;
+    private int points;
 
-	public User(Person person, String userName, String password,
-			String repeatPassword) {
-		checkPasswordsMatch(password, repeatPassword);
-		this.person = person;
-		this.userName = new NotBlankString(userName,
-				INVALID_USERNAME).value();
-		this.password = new Password(password);
-		this.points = 0;
-		this.purchases = new ArrayList<>();
-	}
+    public User(Person person, String userName, String password,
+                String repeatPassword) {
+        this.password = new Password(password);
+        this.password.checkPasswordsMatch(password, repeatPassword);
+        this.person = person;
+        this.userName = new NotBlankString(userName,
+                INVALID_USERNAME).value();
+        this.points = 0;
+        this.purchases = new ArrayList<>();
+    }
 
-	private void checkPasswordsMatch(String password, String repeatPassword) {
-		if (!password.equals(repeatPassword)) {
-			throw new BusinessException(PASSWORDS_MUST_BE_EQUALS);
-		}
-	}
+    boolean hasPassword(String password) {
+        return this.password.hasPassword(password);
+    }
 
-	boolean hasPassword(String password) {
-		return this.password.equals(new Password(password));
-	}
+    public void changePassword(String currentPassword, String newPassword1,
+                               String newPassword2) {
+        this.password.change(currentPassword, newPassword1, newPassword2);
+    }
 
-	public void changePassword(String currentPassword, String newPassword1,
-			String newPassword2) {
-		if (!hasPassword(currentPassword)) {
-			throw new BusinessException(CAN_NOT_CHANGE_PASSWORD);
-		}
-		checkPasswordsMatch(newPassword2, newPassword1);
+    void newEarnedPoints(int points) {
+        if (points <= 0) {
+            throw new BusinessException(POINTS_MUST_BE_GREATER_THAN_ZERO);
+        }
+        this.points += points;
+    }
 
-		this.password = new Password(newPassword1);
-	}
+    public boolean hasPoints(int points) {
+        return this.points == points;
+    }
 
-	void newEarnedPoints(int points) {
-		if (points <= 0) {
-			throw new BusinessException(POINTS_MUST_BE_GREATER_THAN_ZERO);
-		}
-		this.points += points;
-	}
+    public String userName() {
+        return userName;
+    }
 
-	public boolean hasPoints(int points) {
-		return this.points == points;
-	}
+    public boolean hasName(String aName) {
+        return this.person.hasName(aName);
+    }
 
-	public String userName() {
-		return userName;
-	}
+    public boolean hasSurname(String aSurname) {
+        return this.person.aSurname(aSurname);
+    }
 
-	public boolean hasName(String aName) {
-		return this.person.hasName(aName);
-	}
+    public boolean hasUsername(String aUserName) {
+        return this.userName.equals(aUserName);
+    }
 
-	public boolean hasSurname(String aSurname) {
-		return this.person.aSurname(aSurname);
-	}
+    void newPurchase(Sale sale, int pointsWon) {
+        this.newEarnedPoints(pointsWon);
+        this.purchases.add(sale);
+    }
 
-	public boolean hasUsername(String aUserName) {
-		return this.userName.equals(aUserName);
-	}
+    String email() {
+        return this.person.email();
+    }
 
-	void newPurchase(Sale sale, int pointsWon) {
-		this.newEarnedPoints(pointsWon);
-		this.purchases.add(sale);
-	}
+    public Map<String, Object> toMap() {
+        return Map.of("id", this.id);
+    }
 
-	String email() {
-		return this.person.email();
-	}
+    Long id() {
+        return id;
+    }
 
-	public Map<String, Object> toMap() {
-		return Map.of("id", this.id);
-	}
-
-	Long id() {
-		return id;
-	}
-
-	public UserProfile toProfile() {
-		return new UserProfile(this.person.fullName(), this.userName,
-				this.person.email(), this.points);
-	}
+    public UserProfile toProfile() {
+        return new UserProfile(this.person.fullName(), this.userName,
+                this.person.email(), this.points);
+    }
 }
